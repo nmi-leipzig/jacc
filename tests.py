fcb954bba14c4151026fa2f1a84f68489ab56075
Wakiya <ws95zike@studserv.uni-leipzig.de>
2021-05-24 01:14:08 +0200

    added more tests for the object relational infrastructure

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 import pathlib
-from otto import *
-from fpgaobjects import FPGAModel, PrimitiveAttribute, Primitive
+
+from fpgaobjects import FPGAModel, PrimitiveAttribute, Primitive, check_significant_digits
 
 
 paths = {"dummy_fpga.xml": pathlib.Path(__file__).parent.joinpath("fpga_models", "dummy_fpga.xml"),
@@ -22,34 +22,23 @@ class PathTest(unittest.TestCase):
             self.assertTrue(self.file_dict[key].readable())
 
 
-# Test Case for the FPGAModel class
-class FPGAModelTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.dummy_model = FPGAModel.from_xml(paths["dummy_fpga.xml"])
-
-    # Test if attributes were read correctly from xml
-    def test_dummy_model_attribute(self):
-        self.assertEqual(self.dummy_model.name, "dummy")
-        self.assertEqual(self.dummy_model.mmcm_f_in_min, 10)
-
-    # Test if values for specific limitations are validated correctly
-    def test_dummy_model_value_verification(self):
-        self.assertTrue(self.dummy_model.validate_mmcm_input_frequency(55))
-        self.assertFalse(self.dummy_model.validate_pll_vco(799))
-
-
 # Test Cases for the PrimitiveAttribute classes (and others that inherit from PrimitiveAttribute)
 class PrimitiveAttributeTest(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
+    def test_significant_digit_check(self):
+        self.assertTrue(check_significant_digits(1.2300000001, 3))
+        self.assertTrue(check_significant_digits(136.00000001, 3))
+        self.assertFalse(check_significant_digits(135.1110, 4))
+        self.assertFalse(check_significant_digits(1.111, 2))
+
     def test_range_attribute(self):
-        attribute = PrimitiveAttribute.from_xml("<primitive_attribute>\
-            <attribute_type>RangeAttribute</attribute_type>\
+        attribute = PrimitiveAttribute.from_xml("<primitive_attribute type=\"RangeAttribute\">\
             <name>clkfbout_mult_f</name>\
-            <type>float</type>\
+            <num_type>float</num_type>\
             <default>5.000</default>\
-            <step_size>0.001</step_size> <!-- needs to be overlooked -->\
+            <significant_digits>3</significant_digits>\
             <start>2.000</start>\
             <end>64.000</end>\
         </primitive_attribute>")
@@ -60,14 +49,13 @@ class PrimitiveAttributeTest(unittest.TestCase):
 
         # Set value to something invalid and test
         attribute.set_value("64.001")
-        self.assertTrue(attribute.is_valid())
+        self.assertFalse(attribute.is_valid())
 
     # Test creation of ListAttribute from xml and test validation function
     def test_list_attribute(self):
-        attribute = PrimitiveAttribute.from_xml("<primitive_attribute>\
-            <attribute_type>ListAttribute</attribute_type>\
-            <name>bandwith</name>\
-            <type>str</type>\
+        attribute = PrimitiveAttribute.from_xml("<primitive_attribute type=\"ListAttribute\">\
+            <name>bandwidth</name>\
+            <num_type>str</num_type>\
             <default>OPTIMIZED</default>\
             <values>\
                 <value>OPTIMIZED</value>\
@@ -76,25 +64,30 @@ class PrimitiveAttributeTest(unittest.TestCase):
             </values>\
         </primitive_attribute>")
 
+        # Test "values" attribute
+        self.assertTrue(attribute.values == ["OPTIMIZED", "HIGH", "LOW"])
+
         # Set value to something valid and test
         attribute.set_value("OPTIMIZED")
         self.assertTrue(attribute.is_valid())
 
         # Set value to something invalid and test
         attribute.set_value("OTTO")
-        self.assertTrue(attribute.is_valid())
+        self.assertFalse(attribute.is_valid())
 
 
 # Test Cases for the Primitive class
 # This also tests the existing xml files for Primitives
 class PrimitiveTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.mmcme2_base_primitive = Primitive.from_xml(paths["mmcme2_base_attributes.xml"])
+        with open(paths["mmcme2_base_attributes.xml"]) as file:
+            temp_str = "".join(file.readlines())
+        self.mmcme2_base_primitive = Primitive.from_xml(temp_str)
         # self.plle2_base_primitive = Primitive.from_xml(paths["plle2_base_attributes.xml"])
 
     # Test if all the necessary attributes were loaded from the xml
     def test_mmcme2_base_attributes(self):
-        for attribute_name in ["bandwith", "clkfbout_mult_f", "clkfbout_phase", "clkin1_period", "clkout0_divide_f",
+        for attribute_name in ["bandwidth", "clkfbout_mult_f", "clkfbout_phase", "clkin1_period", "clkout0_divide_f",
                                "clkout1_divide", "clkout2_divide", "clkout3_divide", "clkout4_divide",
                                "clkout5_divide", "clkout0_duty_cycle", "clkout1_duty_cycle", "clkout2_duty_cycle",
                                "clkout3_duty_cycle", "clkout4_duty_cycle", "clkout5_duty_cycle", "clkout6_duty_cycle",
@@ -102,6 +95,24 @@ class PrimitiveTest(unittest.TestCase):
                                "clkout5_phase", "clkout6_phase", "clkout4_cascade", "divclk_divide", "ref_jitter1",
                                "startup_wait"]:
             self.assertIn(attribute_name, self.mmcme2_base_primitive.__dict__)
+
+
+# Test Case for the FPGAModel class
+class FPGAModelTest(unittest.TestCase):
+    def setUp(self) -> None:
+        with open(paths["dummy_fpga.xml"]) as file:
+            temp_str = "".join(file.readlines())
+        self.dummy_model = FPGAModel.from_xml(temp_str)
+
+    # Test if attributes were read correctly from xml
+    def test_dummy_model_attribute(self):
+        self.assertEqual(self.dummy_model.name, "dummy")
+        self.assertEqual(self.dummy_model.mmcm_f_in_min, 10)
+
+    # Test if values for specific limitations are validated correctly
+    def test_dummy_model_value_verification(self):
+        self.assertTrue(self.dummy_model.validate_mmcm_input_frequency(55))
+        self.assertFalse(self.dummy_model.validate_pll_vco(799))
 
 
 if __name__ == '__main__':
